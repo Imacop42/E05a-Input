fdd5a741f5b44d4530942c99f1a5d9058cae918e
Imacop42 <60049895+Imacop42@users.noreply.github.com>
2020-02-12 21:23:51 -0500

    got the ship to move

--- Scripts/Ship.gd
+++ Scripts/Ship.gd
@@ -5,4 +5,11 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("LEFT"):
+		position.x = position.x - 10
+	if Input.is_action_pressed("FORWARD"):
+		position.y = position.y - 10
+	if Input.is_action_pressed("BACKWARD"):
+		position.y = position.y + 10
+	if Input.is_action_pressed("RIGHT"):
+		position.x = position.x + 10
